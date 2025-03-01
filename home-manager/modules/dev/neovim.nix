b3bf5a3477141d64b40a36b5b6d56207bd72ebe8
quentin <quentin.le-nestour@outlook.com>
2025-03-01 18:32:09 +0100

    added obsidian keybinds

--- home-manager/modules/dev/neovim.nix
+++ home-manager/modules/dev/neovim.nix
@@ -68,7 +68,7 @@ in {
             daily_notes = {
               folder = "11-Daily";
               dates_format = "%d-%m-%Y";
-              default_tags = [ "daily_notes" ];
+              default_tags = ["daily_notes"];
             };
             workspaces = [
               {
@@ -109,6 +109,63 @@ in {
             mode = "n";
             silent = false;
             action = ":%!jq .<CR>";
+          }
+          {
+            key = "<leader>nsn";
+            mode = "n";
+            silent = false;
+            action = ":ObsidianSearch<CR>";
+          }
+          {
+            key = "<leader>nst";
+            mode = "n";
+            silent = false;
+            action = ":ObsidianTags<CR>";
+          }
+
+          {
+            key = "<leader>fo";
+            mode = "n";
+            silent = false;
+            action = ":ObsidianSearch<CR>";
+          }
+          {
+            key = "<leader>nn";
+            mode = "n";
+            silent = false;
+            action = ":ObsidianNew<CR>";
+          }
+          {
+            key = "<leader>nqs";
+            mode = "n";
+            silent = false;
+            action = ":ObsidianQuickSwitch<CR>";
+          }
+          {
+            key = "<leader>ndt";
+            mode = "n";
+            silent = false;
+            action = ":ObsidianToday<CR>";
+          }
+
+          {
+            key = "<leader>ndd";
+            mode = "n";
+            silent = false;
+            action = ":ObsidianTomorrow<CR>";
+          }
+          {
+            key = "<leader>ndy";
+            mode = "n";
+            silent = false;
+            action = ":ObsidianYesterday<CR>";
+          }
+
+          {
+            key = "<leader>nda";
+            mode = "n";
+            silent = false;
+            action = ":ObsidianDailies<CR>";
           }
         ];
 

94b1eacdffa40e7e67a6643f5d8783d0f850305e
quentin <quentin.le-nestour@outlook.com>
2025-03-15 14:48:06 +0100

    added harpoon

--- home-manager/modules/dev/neovim/plugins/harpoon.nix
+++ home-manager/modules/dev/neovim/plugins/harpoon.nix
@@ -1,6 +1,13 @@
 {
-  programs.nvf.settings.vim.navigation.harpoon = {
+  programs.nvf.settings.vim = {
+    navigation.harpoon = {
       enable = true;
-
+      mappings = {
+        file1 = "<C-q>"; 
+        file2 = "<C-s>"; 
+        file3 = "<C-d>"; 
+        file4 = "<C-f>"; 
+      };
     };
+  };
 }

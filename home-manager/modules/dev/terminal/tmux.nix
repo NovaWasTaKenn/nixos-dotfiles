5febf6e020fd3fac39bc6acdc08580b353ab13fb
quentin <quentin.le-nestour@outlook.com>
2025-03-16 19:21:26 +0100

    changed tmux window nav

--- home-manager/modules/dev/terminal/tmux.nix
+++ home-manager/modules/dev/terminal/tmux.nix
@@ -1,38 +1,57 @@
-{ config, pkgs, lib, ...}:
-
 {
-    programs.tmux = 
-    {
-        enable = true;  
-        keyMode = "vi";
-        prefix = "C-Space";
-        baseIndex = 1;
-        escapeTime = 0;
-        historyLimit = 10000;
-        terminal = "tmux-256color";
-        plugins = with pkgs; [
-          #tmuxPlugins.sensible By default on nix package
-          tmuxPlugins.vim-tmux-navigator
-          tmuxPlugins.yank
-          tmuxPlugins.catppuccin
-          tmuxPlugins.tmux-floax
-          #{
-          #  plugin = tmuxPlugins.resurrect;
-          #  extraConfig = "set -g @resurrect-strategy-nvim 'session'";
-          #}
-        ];
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  programs.tmux = {
+    enable = true;
+    keyMode = "vi";
+    prefix = "C-Space";
+    baseIndex = 1;
+    escapeTime = 0;
+    historyLimit = 10000;
+    terminal = "tmux-256color";
+    plugins = with pkgs; [
+      #tmuxPlugins.sensible By default on nix package
+      tmuxPlugins.vim-tmux-navigator
+      tmuxPlugins.yank
+      tmuxPlugins.catppuccin
+      tmuxPlugins.tmux-floax
+      #{
+      #  plugin = tmuxPlugins.resurrect;
+      #  extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+      #}
+    ];
 
-        extraConfig = lib.strings.concatStringsSep "\n" [
-          "bind -n M-H previous-window"
-          "bind -n M-L next-window"
-          "bind-key -T copy-mode-vi v send-keys -X begin-selection"
-          "bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle"
-          "bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel"
-          "bind '\"' split-window -v -c \"#{pane_current_path}\""
-          "bind % split-window -h -c \"#{pane_current_path}\""
+    extraConfig = lib.strings.concatStringsSep "\n" [
+      "bind -n M-h previous-window"
+      "bind -n M-l next-window"
 
-          #"set -g @catpuccin_flavour 'latte'"
-          "set -g @floax-bind '-n M-p'" #root bind for tmux floax
-        ]; 
-    };
+      "bind | split-window -h"
+      "bind _ split-window -v"
+      "unbind '\"'"
+      "unbind %"
+
+      "bind-key à select-window -t :0"
+      "bind-key & select-window -t :1"
+      "bind-key é select-window -t :2"
+      "bind-key \" select-window -t :3"
+      "bind-key ' select-window -t :4"
+      "bind-key ( select-window -t :5"
+      "bind-key - select-window -t :6"
+      "bind-key è select-window -t :7"
+      "bind-key _ select-window -t :8"
+      "bind-key ç select-window -t :9"
+
+      "bind-key -T copy-mode-vi v send-keys -X begin-selection"
+      "bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle"
+      "bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel"
+      "bind '\"' split-window -v -c \"#{pane_current_path}\""
+      "bind % split-window -h -c \"#{pane_current_path}\""
+
+      #"set -g @catpuccin_flavour 'latte'"
+      "set -g @floax-bind '-n M-p'" #root bind for tmux floax
+    ];
+  };
 }

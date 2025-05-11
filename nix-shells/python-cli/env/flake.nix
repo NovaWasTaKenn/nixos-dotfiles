9dba3d2b042e375c3d70529c1153f536cd1a9d74
quentin <quentin.le-nestour@outlook.com>
2025-05-11 20:34:28 +0200

    renamed project template

--- nix-shells/python-cli/env/flake.nix
+++ nix-shells/python-cli/env/flake.nix
@@ -1,17 +1,17 @@
 {
-  description = "Ephemeral python dev env";
+  description = "Ephemeral scala dev env";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nvimConfigs = {
-      url = "path:../../modules/home-manager/dev/neovim";
+      url = "path:../dev/neovim";
       inputs.nixpkgs.follows = "nixpkgs"; # Suis le nixpkgs défini précédemment ou alors nixpkgs alias nix unstable ????
     };
   };
 
   outputs = inputs @ {...}: let
     system = "x86_64-linux";
-    pkgs = import inputs.pkgs {
+    pkgs = import inputs.nixpkgs {
       system = system;
       overlays = [
         (final: prev: {
@@ -27,13 +27,31 @@
     devShells."x86_64-linux".default = pkgs.mkShell {
       packages = with pkgs; [
         python313
-        #python313Packages.click
-        #python313Packages.cock
+        python313Packages.click
+        python313Packages.cock
         pythonNvim
       ];
 
-      shellHook = "echo 'welcome to a python dev shell'";
+      shellHook = "echo 'Welcome to a python dev env'";
       #ENV_VAR = "";
+    };
+
+    packages.${system}.default = pkgs.python3Packages.buildPythonPackage rec {
+      pname = "myPackage";
+      version = "0.1.0";
+
+      src = ./.;
+
+      propagatedBuildInputs = [
+        pkgs.python3Packages.click
+      ];
+
+      meta = with pkgs.lib; {
+        description = "";
+        homepage = "";
+        license = licenses.mit;
+        maintainers = with maintainers; [NovaWasTakenn];
+      };
     };
   };
 }
